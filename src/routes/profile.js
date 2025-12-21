3f079a35549e2525e96ab1b3fcfb36a2451b0655
VihaShahh <vihashah1103@gmail.com>
2025-12-21 16:17:14 +0530

    reset password api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,10 @@ import express from "express"
 const profileRouter = express.Router()
 import { userAuth } from "../middleware/middleware.js"
 import { validateProfileData } from "../utils/validation.js";
+import User from "../models/user.js"
+import bcrypt from "bcrypt"
+import validator from "validator";
+
 // =========================
 // GET: User Profile (Protected)
 // =========================  
@@ -14,6 +18,9 @@ profileRouter.get("/profile/view", userAuth, (req, res) => {
     });
 });
 
+// =========================
+// PATCH: Update Profile (Protected)
+// =========================  
 profileRouter.patch("/profile/update", userAuth, async (req, res) => {
     try {
         if (!validateProfileData(req)) {
@@ -21,15 +28,13 @@ profileRouter.patch("/profile/update", userAuth, async (req, res) => {
         }
 
         const loggedInUser = req.user;
-        console.log(loggedInUser);
 
         Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]))
         await loggedInUser.save()
 
-        console.log(loggedInUser)
         res.status(200).json({
             success: true,
-            message: "Profile update request is valid",
+            message: "Profile update successfully",
             data: loggedInUser
         });
 
@@ -41,4 +46,43 @@ profileRouter.patch("/profile/update", userAuth, async (req, res) => {
     }
 });
 
-export default profileRouter
+// =========================
+// PATCH: Reset Password by Email
+// =========================  
+profileRouter.patch("/profile/resetPassword", async (req, res) => {
+    try {
+        const { emailId, newPassword } = req.body;
+
+        if (!emailId || !newPassword) {
+            throw new Error("Email and newPassword are required");
+        }
+
+        // Validate BEFORE hashing
+        if (!validator.isStrongPassword(newPassword)) {
+            throw new Error("Password must be strong");
+        }
+
+        const user = await User.findOne({ emailId });
+        if (!user) {
+            throw new Error("Invalid Email");
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        user.password = hashedPassword;
+        await user.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Password reset successful"
+        });
+
+    } catch (err) {
+        return res.status(400).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
+
+export default profileRouter;
